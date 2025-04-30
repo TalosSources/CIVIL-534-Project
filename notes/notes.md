cafdaddefa7ad451a73a47f01c8f68e657f0aefe
Talos <ismail.sahbane@epfl.ch>
2025-04-30 17:42:28 +0200

    refactor, add new shell code for milestone_2_cycles

---
+++ notes/notes.md
@@ -0,0 +1,5 @@
+# ideas
+* compute the importance metrics of the nodes used in milestone 1
+* correlation between different metrics
+* say in limitations that the network structure is just a proxy for policy impact, and that we loose a lot of information by converting to a network
+* find communities relevant to the policy, find policy for those communities 
---
+++ world3_network.py
@@ -0,0 +1,16 @@
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+json_file = open("world3-03_variables.json")
+w3_vars = json.loads(json_file.read())
+json_file.close()
+
+G = nx.DiGraph()
+for name, val in w3_vars.items():
+    G.add_node(name, var_type=val["type"])
+    if val["dependencies"] is not None:
+        G.add_edges_from([(dep, name) for dep in val["dependencies"]])
+
+print(f"G: {G}")
+nx.draw(G)
